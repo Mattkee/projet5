31f8f2ab3912d501b2d86f30cad8d730e7f4b330
Mattkee <tkee@hotmail.fr>
2018-01-03 19:01:22 +0100

    add new func spacetoview

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -14,18 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         //1
         NotificationCenter.default.addObserver(self, selector: #selector(deviceOrientationDidChange), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
-        
-        let view1 = UIStackView(frame: CGRect(x: 0, y: 0, width: shareView.frame.width, height: shareView.frame.height))
-        shareView.addSubview(view1)
-        
-        
-        let viewcontent = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-        viewcontent.backgroundColor = UIColor.cyan
-        view1.addSubview(viewcontent)
-        
-        let viewcontent1 = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-        viewcontent1.backgroundColor = UIColor.green
-        view1.addSubview(viewcontent1)
+        displayImageView()
         
     }
     
@@ -54,11 +43,57 @@ class ViewController: UIViewController {
             print("Portrait upside down")
         }
     }
-
+    
     @IBOutlet weak var shareView: UIView!
     @IBOutlet weak var textLabel: UILabel!
     
-    
+    func displayImageView(){
+        
+        let view1 = UIImageView(image: #imageLiteral(resourceName: "croixbleu"))
+        shareView.addSubview(view1)
+        
+        let view2 = UIImageView(image: #imageLiteral(resourceName: "croixbleu"))
+        shareView.addSubview(view2)
+        
+        let view3 = UIImageView(image: #imageLiteral(resourceName: "recbleu"))
+        shareView.addSubview(view3)
+        
+        
+        let tabView = [view1,view2]
+        
+        
+        let stackView = UIStackView(arrangedSubviews: tabView)
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        let princView = [stackView,view3]
+        
+        let stackViewSecond = UIStackView(arrangedSubviews: princView)
+        stackViewSecond.axis = .vertical
+        stackViewSecond.distribution = .fillEqually
+        stackViewSecond.alignment = .fill
+        stackViewSecond.spacing = 8
+        stackViewSecond.translatesAutoresizingMaskIntoConstraints = false
+        shareView.addSubview(stackViewSecond)
+        let secondViewsDictionary = ["stackViewSecond":stackViewSecond]
+        let stackViewSecond_H = NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-10-[stackViewSecond]-10-|",  //horizontal constraint 10 points from left and right side
+            options: NSLayoutFormatOptions(rawValue: 0),
+            metrics: nil,
+            views: secondViewsDictionary)
+        let stackViewSecond_V = NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-10-[stackViewSecond]-10-|", //vertical constraint 10 points from top and bottom
+            options: NSLayoutFormatOptions(rawValue:0),
+            metrics: nil,
+            views: secondViewsDictionary)
+        shareView.addConstraints(stackViewSecond_H)
+        shareView.addConstraints(stackViewSecond_V)
+        
+    }
     
 }
 
